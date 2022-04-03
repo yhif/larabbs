b6ee8d5a5f8207f92dde66986c2dea00e0bfd7e4
coke <daiyihui0520@gmail.com>
2022-04-04 00:04:13 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Auth::routes();
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
